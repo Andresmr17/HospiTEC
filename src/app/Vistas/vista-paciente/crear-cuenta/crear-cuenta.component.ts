38d0509351ac1e91f49c3281b83f01da9959f438
Andrés <andresmolina@estudiantec.cr>
2024-05-20 08:33:12 -0600

    Commit 4
    Modificaciones en la pantalla de crear paciente.
    Avances en la pantalla de Reservaciones

--- src/app/Vistas/vista-paciente/crear-cuenta/crear-cuenta.component.ts
+++ src/app/Vistas/vista-paciente/crear-cuenta/crear-cuenta.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-crear-cuenta',
   templateUrl: './crear-cuenta.component.html',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   styleUrls: ['./crear-cuenta.component.css']
 })
--- src/app/Vistas/vista-paciente/paciente-menu/gestion-reservacion/gestion-reservacion.component.ts
+++ src/app/Vistas/vista-paciente/paciente-menu/gestion-reservacion/gestion-reservacion.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-gestion-reservacion',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './gestion-reservacion.component.html',
   styleUrl: './gestion-reservacion.component.css'
 })
 export class GestionReservacionComponent {
+
+  Nombre: string ="";
+  Cedula: string ="";
+  Fecha: string ="";
+  Procedimientos: string ="";
+
+
+  crearCuenta(Nombre: string, Cedula: string, Fecha: string, Procedimientos: string) {
+    const data = JSON.stringify({Nombre, Cedula, Fecha, Procedimientos});
+    console.log(data);
+  }
 
 }

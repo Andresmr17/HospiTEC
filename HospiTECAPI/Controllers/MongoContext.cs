9f267555aad50b82f44e55780fb7dcaa514acd41
Andrés <andresmolina@estudiantec.cr>
2024-06-05 00:34:20 -0600

    Commit 2
    Fix mongo

--- HospiTECAPI/Controllers/MongoContext.cs
+++ HospiTECAPI/Controllers/MongoContext.cs
@@ -1,27 +1,26 @@
+
 using MongoDB.Driver;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace HospiTECAPI.Controllers;
 
-namespace HospiTECAPI.Controllers
+public class MongoContext
 {
-    public class MongoContext
+    private IMongoDatabase db;
+    public MongoClient client;
+
+    public IMongoDatabase getDb()
     {
-        private IMongoDatabase db;
-        public MongoClient client;
-
-        public IMongoDatabase getDb()
-        {
-            return this.db;
-        }
-
-        public MongoContext()
-        {
-            var connectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING");
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                throw new InvalidOperationException("The MongoDB connection string is not set in the environment variables.");
-            }
-            client = new MongoClient(connectionString);
-            db = client.GetDatabase("proyectoBases");
-        }
+        return this.db;
     }
+
+    public MongoContext()
+    { 
+        client = new MongoClient("");
+        db = client.GetDatabase("proyectoBases");
+    }
+
+   
 }

8ca93feb4ed8c5b3d81e12c939c3361c1107285f
mrr79 <marianarojas2197@gmail.com>
2024-06-05 01:37:21 -0600

    camas y salones ::borrado

--- src/app/Vistas/camas/camas.component.ts
+++ src/app/Vistas/camas/camas.component.ts
@@ -1,24 +1,31 @@
 import { Component } from '@angular/core';
-import {NgForOf} from "@angular/common";
-import {ComunicationService} from "../../Servicios/comunication.service";
+import { NgForOf } from "@angular/common";
+import { ComunicationService } from "../../Servicios/comunication.service";
 
 export interface Camas {
   idCama: number;
   nombreSalon: string;
   estadoUCI: boolean;
 }
+
+export interface Salon {
+  nombreSalon: string;
+  capacidadCamas: number;
+  tipoDeSalon: string;
+  numeroDePiso: number;
+}
+
 @Component({
   selector: 'app-camas',
   standalone: true,
-    imports: [
-        NgForOf
-    ],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './camas.component.html',
   styleUrl: './camas.component.css'
 })
 export class CamasComponent {
-  constructor(private servicio:ComunicationService) {
-  }
+  constructor(private servicio: ComunicationService) { }
   dataSource: Camas[] = [
     {
       "idCama": 1,
@@ -36,34 +43,62 @@ export class CamasComponent {
       "estadoUCI": true
     }
   ];//aca se guardan los datos solicitados del servidor
-//variables para determinar si es un udptae o un insert.
-  idCama = 0
+
+  idCama = 0;
   modalVisible = false;
   tipoModal = 2; //me indica el tipo de modal, si es añadido o update
   isReadonly = true; //para habilitar el id
-  /*metodo que me permite modificar registros
-  * por temas de que solo se puede usar un "modal" por componente
-  * se utilizara este metodo tamto para modificar como para añadir
-  * utilizando el mismo modal*/
+
   modificarCamas(index: number, tipodeModal: number) {
-    this.tipoModal =tipodeModal
+    this.tipoModal = tipodeModal;
     this.isReadonly = true;
-    //modificar mas adelante
     const salonSeleccionado = this.dataSource[index];
-    this.idCama= salonSeleccionado.idCama;
-    //id del profesor
+    this.idCama = salonSeleccionado.idCama;
     this.modalVisible = true;
     console.log('Se ha presionado el botón de modificar para el elemento en el índice:', this.idCama);
   }
 
-  //metodo para modificar los registros en base al index
   eliminarCama(index: number) {
     const camaSeleccionada = this.dataSource[index];
     console.log('Se ha presionado el botón de eliminar para el elemento en el índice:', index);
-    this.servicio.deleteCama(camaSeleccionada.idCama).subscribe(
+    // Obtener los salones
+    this.servicio.getSalones().subscribe(
+      (salones: Salon[]) => {
+        // Buscar el salón correspondiente
+        const salon = salones.find((salon: Salon) => salon.nombreSalon === camaSeleccionada.nombreSalon);
+        if (salon) {
+          // Actualizar la capacidad de camas
+          salon.capacidadCamas -= 1;
+          this.servicio.putSalon(salon.nombreSalon, salon).subscribe(
+            response => {
+              console.log('Capacidad del salón actualizada:', response);
+              // Eliminar la cama después de actualizar el salón
+              this.deleteCamaYActualizarDataSource(camaSeleccionada.idCama, index);
+            },
+            error => {
+              console.error('Error al actualizar la capacidad del salón:', error);
+              // Maneja el error adecuadamente aquí
+            }
+          );
+        } else {
+          // Eliminar la cama directamente si no se encuentra el salón
+          this.deleteCamaYActualizarDataSource(camaSeleccionada.idCama, index);
+          this.obtenerEquipo();
+        }
+      },
+      error => {
+        console.error('Error al obtener los salones:', error);
+        // Maneja el error adecuadamente aquí
+      }
+    );
+  }
+
+  private deleteCamaYActualizarDataSource(idCama: number, index: number) {
+    this.servicio.deleteCama(idCama).subscribe(
       response => {
         console.log('Cama eliminada:', response);
         this.dataSource.splice(index, 1); // Eliminar el registro del dataSource
+        this.obtenerEquipo();
       },
       error => {
         console.error('Error al eliminar la cama:', error);
@@ -71,65 +106,58 @@ export class CamasComponent {
       }
     );
   }
-  guardarCambios() { //metodo para el post o update
-    // Aquí puedes acceder a los datos del formulario usando el objeto 'formulario.value'
-    const idCamaElement = (document.getElementById('idCama') as HTMLInputElement).value.trim();
-    console.log('el id salon antes de parsear es :', idCamaElement);
-    const idCama1 = parseInt(idCamaElement, 10);
-    console.log('el id salon despues de parsear es :', idCama1);
-    const nombreSalon1 = (document.getElementById('nombreSalon') as HTMLInputElement).value;
-    const esUIC =((document.getElementById('UIC') as HTMLSelectElement).value === 'true');
 
-    //esta es la data que se va a enviar
-    const datatoSend1={
+  guardarCambios() {
+    const idCamaElement = (document.getElementById('idCama') as HTMLInputElement).value.trim();
+    const idCama1 = parseInt(idCamaElement, 10);
+    const nombreSalon1 = (document.getElementById('nombreSalon') as HTMLInputElement).value;
+    const esUIC = ((document.getElementById('UIC') as HTMLSelectElement).value === 'true');
+
+    const datatoSend1 = {
       Idcama: idCama1,
       Nombresalon: nombreSalon1,
       Estadouci: esUIC
     }
-    //dependiendo si es 1 o 2 es un get o un post
 
-    console.log('el id salon es :', datatoSend1.Idcama);
-    if(this.tipoModal ==1){//si el tipo de modal es 1 entonces es un post
+    if (this.tipoModal == 1) {
       this.servicio.postCamas(datatoSend1).subscribe(
         response => {
           console.log('Datos enviados a posgress', response);
+          this.obtenerEquipo();
         },
         error => {
           console.error('Error al enviar datos al servidor:', error);
           // Maneja el error adecuadamente aquí
         }
       );
-    }
-    else{ //si es 0 entonces es un update
+    } else {
       this.servicio.putCamas(datatoSend1.Idcama, datatoSend1)
         .subscribe(
           () => {
             console.log('La cama se actualizó correctamente.');
-            // Realizar cualquier otra acción necesaria después de la actualización
+            this.obtenerEquipo();
           },
           error => {
             console.error('Error al actualizar la cama:', error);
-            // Manejar el error adecuadamente
           }
         );
     }
   }
-  addRegistro(numero:number){
-    this.tipoModal=numero //setea el tipo de modal a un modal de
-    this.idCama=0;
-    this.isReadonly=false
-    //añadido de registros
+
+  addRegistro(numero: number) {
+    this.tipoModal = numero;
+    this.idCama = 0;
+    this.isReadonly = false;
   }
-  obtenerEquipo(){ //Medoto get que obtiene todas las camas.
+
+  obtenerEquipo() {
     this.servicio.getCamas().subscribe(
       response => {
         console.log('Datos recibidos de posgress', response);
-
-        this.dataSource = response; //aca igualo a mi datasource
+        this.dataSource = response;
       },
       error => {
         console.error('Error al enviar datos al servidor:', error);
-        // Maneja el error adecuadamente aquí
       }
     );
   }
